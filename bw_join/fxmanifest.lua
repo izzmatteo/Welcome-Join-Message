580d304c4aacc3937feafc20831698515baae4e7
izzmatteo <83980937+izzmatteo@users.noreply.github.com>
2021-05-27 17:55:36 +0200

    Add files via upload

---
+++ bw_join/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+games {'gta5'}
+
+dependency 'es_extended'
+
+
+author 'izz Matteo'
+description 'Join Message'
+version '1.0'
+
+
+server_scripts {
+	'main.lua'
+}
+
+
+client_scripts {
+	'client.lua'
+}
+
